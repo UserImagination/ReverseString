ce198cf57be29964ca5cabc0433272c730d8ccb3
hawk2 <joehaven@gmail.com>
2024-03-03 15:50:49 -0800

    Initial Commit

---
+++ ReverseString/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ReverseString
+//
+//  Created by Joseph Craig on 3/3/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
