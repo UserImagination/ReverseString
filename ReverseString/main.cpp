5cacedccec321ff01beb9cc7c4e731aac58ad0b8
hawk2 <joehaven@gmail.com>
2024-03-03 17:23:40 -0800

    ok

--- ReverseString/main.cpp
+++ ReverseString/main.cpp
@@ -1,14 +1,27 @@
-//
-//  main.cpp
-//  ReverseString
-//
-//  Created by Joseph Craig on 3/3/24.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    string user;
+    string phrase;
+    size_t pos;
+    
+    // input words OR phrases, separated by linebreaks (press enter)
+    // when you're done, type in "Done", "done", or "d" as the last string
+    while (true) {
+        getline(cin, user);
+        if (user == "Done" || user == "done" || user == "d") {
+            break;
+        }
+        for (pos = user.length() - 1; pos != string::npos; pos--) {
+            phrase += user.at(pos);
+        }
+        // keep each string on its original line, so that we are only
+        // reversing strings, and not the linebreaks
+        phrase += "\n";
+    }
+    cout << phrase;
+    
     return 0;
 }
